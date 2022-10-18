6e822e6aaca98634ac04a52c62910dc299903adc
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2022-10-18 21:28:01 +0200

    meuu

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '23c95bbfba6681ed0a3bb045f8c95f0911ca1d34',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '23c95bbfba6681ed0a3bb045f8c95f0911ca1d34',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
